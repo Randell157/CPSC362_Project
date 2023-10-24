1713d7b8f9b983a22a874c86b901108bf17abba2
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2023-10-23 19:03:40 -0700

    added matrix size, matrix creation, and changing matrix size

--- JS/index.js
+++ JS/index.js
@@ -3,15 +3,23 @@
 //Last Edit: 10/12/23
 
 //the size of the left matrix
-let leftMatrixRowSize = 1, leftMatrixColSize = 1;
+const leftMatrixRowSize = document.getElementById("leftMatRowSize");
+const leftMatrixColSize = document.getElementById("leftMatColSize");
 //the operation (+, -, /, *)
-let myOp = '+';
+let myOp = document.getElementById("myOp");
 //the size of the right matrix
-let rightMatrixRowSize = 1, rightMatrixColSize = 1;
+const rightMatrixRowSize = document.getElementById("rightMatRowSize");
+const rightMatrixColSize = document.getElementById("rightMatColSize");
 //the max size a matrix can be
 const maxSize = 10;
 //the smallest size a matrix can be
 const minSize = 1;
+
+//the left matrix button
+let leftButton = document.getElementById("leftButton");
+
+//the right matrix button
+let rightButton = document.getElementById("rightButton");
 
 //the left and right matrices
 let leftMatrix = [];
@@ -20,32 +28,91 @@ let rightMatrix = [];
 //the answer matrix
 let answerMatrix = [];
 
-//TODO: make it so that text boxes are spawned in the left matrix
+//creates the left matrix
 function UserEntersLeftMatrixSize()
 {
-    for (let i = 0; i < leftMatrixRowSize; i++)
+    for (let i = 0; i < Number(leftMatrixRowSize.value); i++)
     {
-        for (let j = 0; i < leftMatrixColSize; i++)
+        for (let j = 0; j < Number(leftMatrixColSize.value); j++)
         {
-            console.log(i);
-            console.log(j);
+            let currBox = document.createElement("INPUT");
+            currBox.setAttribute("type", "number");
+            currBox.setAttribute("value", 0);
+            currBox.setAttribute("class", "LeftMatVal")
+            leftMatrix.push(currBox);
+            document.body.appendChild(currBox);
         }
     }
+
+    leftMatrixColSize.disabled = true;
+    leftMatrixRowSize.disabled = true;
+
+    leftButton.setAttribute("onclick", "UserChangesLeftMatrixSize()");
+    leftButton.innerHTML = "Change Left Size";
 }
 
-//TODO: make it so that text boxes are spawned in the right matrix
+//creates the right matrix
 function UserEntersRightMatrixSize()
 {
-    for (let i = 0; i < rightMatrixRowSize; i++)
+    for (let i = 0; i < Number(rightMatrixRowSize.value); i++)
     {
-        for (let j = 0; i < rightMatrixColSize; i++)
+        for (let j = 0; j < Number(rightMatrixColSize.value); j++)
         {
-            console.log(i);
-            console.log(j);
+            let currBox = document.createElement("INPUT");
+            currBox.setAttribute("type", "number");
+            currBox.setAttribute("value", 0);
+            currBox.setAttribute("class", "RightMatVal")
+            rightMatrix.push(currBox);
+            document.body.appendChild(currBox);
         }
     }
+
+    rightMatrixColSize.disabled = true;
+    rightMatrixRowSize.disabled = true;
+
+    rightButton.setAttribute("onclick", "UserChangesRightMatrixSize()");
+    rightButton.innerHTML = "Change Right Size";
 }
 
+//changes the left matrix
+function UserChangesLeftMatrixSize()
+{
+    let leftMatList = document.getElementsByClassName("LeftMatVal");
+    const listSize = leftMatList.length;
+    
+    for (let i = 0; i < Number(listSize); i++)
+    {
+        document.body.removeChild(leftMatList[0]);
+        leftMatrix.pop();
+    }
+
+    leftMatrixColSize.disabled = false;
+    leftMatrixRowSize.disabled = false;
+
+    leftButton.setAttribute("onclick", "UserEntersLeftMatrixSize()");
+    leftButton.innerHTML = "Create Left Matrix";
+}
+
+//changes the right matrix
+function UserChangesRightMatrixSize()
+{
+    let rightMatList = document.getElementsByClassName("RightMatVal");
+    const listSize = rightMatList.length;
+    
+    for (let i = 0; i < Number(listSize); i++)
+    {
+        document.body.removeChild(rightMatList[0]);
+        rightMatrix.pop();
+    }
+
+    rightMatrixColSize.disabled = false
+    rightMatrixRowSize.disabled = false;
+
+    rightButton.setAttribute("onclick", "UserEntersRightMatrixSize()");
+    rightButton.innerHTML = "Create Right Matrix";
+}
+
+/*
 const leftNum = document.getElementById("leftNum");
 const myOperation = document.getElementById("myOp");
 const rightNum = document.getElementById("rightNum");
@@ -77,3 +144,4 @@ function add()
     }
     myOut.innerHTML = String(myAnswer);
 }
+*/
